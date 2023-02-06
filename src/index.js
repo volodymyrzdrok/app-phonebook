17e3b584e24fc4048ad710823847a52442b6ae50
Volodymyr Zdrok <volodymyrzdrok44@gmail.com>
2023-02-06 23:13:12 +0200

    finished withour brouseRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter,HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './components/App';
 import './index.css';
 import { store, persistor } from './redux/store';
